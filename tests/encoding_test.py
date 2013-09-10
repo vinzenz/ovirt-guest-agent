8db31e3c882874ff1a80a5b1f4092322775d1185
Vinzenz Feenstra <vfeenstr@redhat.com>
2013-09-10 13:16:45 +0200

    More encoding tests
    Change-Id: I4c1d740c948e33aed8a04a1013b22276cf7d85e3
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- tests/encoding_test.py
+++ tests/encoding_test.py
@@ -18,17 +18,30 @@ class EncodingTest(TestCaseBase):
         self.assertEquals({u'unicode-key': u'non-unicode value'},
                           _filter_object(non_unicode_value))
 
+    def testNullChar(self):
+        non_unicode_value = {u'unicode-key': '\x00'}
+        self.assertEquals({u'unicode-key': u'\ufffd'},
+                          _filter_object(non_unicode_value))
+
+    def testIllegalInput(self):
+        ILLEGAL_DATA = {u'foo': '\x00data\x00test\xef\xbf\xbf\xef\xbf\xbe\xed'
+                                '\xb1\xb9\xed\xa0\x80'}
+        EXPECTED = {u'foo': u'\ufffddata\ufffdtest\ufffd\ufffd\ufffd\ufffd'}
+        self.assertEqual(EXPECTED, _filter_object(ILLEGAL_DATA))
+
     def testIllegalUnicodeInput(self):
         ILLEGAL_DATA = {u'foo': u'\x00data\x00test\uffff\ufffe\udc79\ud800'}
         EXPECTED = {u'foo': u'\ufffddata\ufffdtest\ufffd\ufffd\ufffd\ufffd'}
         self.assertEqual(EXPECTED, _filter_object(ILLEGAL_DATA))
 
-    def testLlegalUnicodeInput(self):
+    def testLegalUnicodeInput(self):
         LEGAL_DATA = {u'foo': u'?data?test\U00010000'}
         self.assertEqual(LEGAL_DATA, _filter_object(LEGAL_DATA))
 
     def testIllegalUnicodeCharacters(self):
-        INVALID = (u'\u0000', u'\ufffe', u'\uffff', u'\ud800', u'\udc79')
+        INVALID = (u'\u0000', u'\ufffe', u'\uffff', u'\ud800', u'\udc79',
+                   u'\U00000000', '\x00', '\x01', '\x02', '\x03', '\x04',
+                   '\x05')
         for invchar in INVALID:
             self.assertEqual(u'\ufffd', _filter_object(invchar))
 
